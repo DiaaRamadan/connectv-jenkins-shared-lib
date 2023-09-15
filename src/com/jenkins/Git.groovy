c9499e33fa4b6334aa6193eeb4b894221d0e9fe7
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-09-15 19:20:15 +0300

    add git module

---
+++ src/com/jenkins/Git.groovy
@@ -0,0 +1,29 @@
+#!/usr/bin/env groovy
+
+package com.jenkins
+
+class Git implements Serializable {
+
+    def script
+
+    Git(script) {
+        this.script = script
+    }
+
+    def commitVersionUpdated() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'github-token', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            script.sh 'git config --global user.email "jenkins@example.com"'
+            script.sh 'git config --global user.name "jenkins"'
+
+            script.sh 'git status'
+            script.sh 'git branch'
+            script.sh 'git config --list'
+
+            script.sh "git remote set-url origin https://${PASS}@github.com/DiaaRamadan/connectv-jenkins-shared-lib.git"
+            script.sh 'git add .'
+            script.sh 'git commit -m "ci:Version bump"'
+            script.sh 'git push origin HEAD:master'
+        }
+    }
+
+}
---
+++ vars/commitVersionUpdated.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+import com.jenkins.Git
+
+def call() {
+    return new Git(this).commitVersionUpdated()
+}
