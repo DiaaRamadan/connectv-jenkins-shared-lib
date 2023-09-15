6e009bd55f655a8b62ab98ed33ea264aee6ddf2d
diaa ramadan <diaa.ramdan@visionvalley.net>
2023-09-15 23:39:23 +0300

    add docker moodule

---
+++ src/com/jenkins/Docker.groovy
@@ -0,0 +1,28 @@
+#!/usr/bin/env groovy
+
+package com.jenkins
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def dockerBuildImage(String imageName, String path) {
+        script.echo "building the docker image..."
+        script.sh "docker build -t $imageName $path"
+    }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub', usernameVariable: 'USER', passwordVariable: 'PASS')]) {
+            script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
+        }
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
+    }
+
+}
---
+++ vars/dockerPush.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+import com.jenkins.Docker
+
+def call(String imageName, String path) {
+    return new Docker(this).dockerPush(imageName)
+}
